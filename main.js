7bb92b9aa45a868664f3cb1365b4d666fea97621
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-06-13 14:19:13 -0700

    SETUP project. Add background image.

---
+++ main.js
@@ -0,0 +1,24 @@
+const game = new Phaser.Game(1680, 1050, Phaser.CANVAS, 'content');
+
+let background;
+let music;
+
+const mainState = {
+  preload: () => {
+    game.load.image('background', 'graphics/background.jpg');
+    game.load.audio('mainSong', 'sounds/mainSong.mp3');
+  },
+  create: () => {
+    background = game.add.sprite(0, 0, 'background');
+    music = game.add.audio('mainSong');
+    music.volume = 100;
+    music.loop = true;
+    music.play();
+  },
+  update: () => {
+  },
+}
+
+game.state.add('mainState', mainState);
+
+game.state.start('mainState');
